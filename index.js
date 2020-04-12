118b0ee655a3a002fd018b42ecc98b7e47e44b0c
Radu Poleuca <radupoleuca@gmail.com>
2020-04-12 23:14:31 +0300

    closer

---
+++ index.js
@@ -0,0 +1,33 @@
+<script>
+var newSlide;
+
+            var slideIndex = 1;
+            showSlides(slideIndex);
+            
+            function plusSlides(n= +1) {
+                slideIndex = slideIndex + n;
+                showSlides(slideIndex);
+            }
+            
+            function currentSlide(n) {
+              showSlides(slideIndex = n);
+            }
+            
+            function showSlides(n) {
+              clearInterval(newSlide);
+              var i;
+              var slides = document.getElementsByClassName("mySlides");
+              var dots = document.getElementsByClassName("dot");
+              if (n > slides.length) {slideIndex = 1}    
+              if (n < 1) {slideIndex = slides.length}
+              for (i = 0; i < slides.length; i++) {
+                  slides[i].style.display = "none";  
+              }
+              for (i = 0; i < dots.length; i++) {
+                  dots[i].className = dots[i].className.replace(" active", "");
+              }
+              slides[slideIndex-1].style.display = "block";  
+              dots[slideIndex-1].className += " active";
+              newSlide = setInterval(plusSlides, 4000);
+            }
+			</script>
